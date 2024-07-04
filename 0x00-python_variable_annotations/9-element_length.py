ec6fd9a4261f31338fe2ac7f8394689c619de688
lawalTheWest <lwltjdn@gmail.com>
2024-07-04 23:54:27 +0100

    .

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+    Module 9-element_length
+    Returns list
+'''
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''Returns values with appropriate types'''
+    return [(i, len(i)) for i in lst]
