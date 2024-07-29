4a7cc4ba8b1a8fbf11841604fee6296f0c253e5a
lawalTheWest <lwltjdn@gmail.com>
2024-07-29 15:06:13 +0100

    unit testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -34,3 +34,45 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(map, path)
 
         self.assertEqual(str(e.exception)[1:-1], wrong_output)
+
+class TestGetJson(unittest.TestCase):
+    '''
+        Test with json
+    '''
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('test_utils.get_json')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        '''
+            test utils.get_json
+        '''
+        mock_get.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    '''
+        test with memoize
+    '''
+    def test_memoize(self):
+        '''
+            Ttest assert_called_once
+        '''
+        class TestClass:
+            """ class """
+            def a_method(self):
+                ''' method  '''
+                return 42
+
+            @memoize
+            def a_property(self):
+                ''' property '''
+                return self.a_method()
+        with patch.object(TestClass, "a_method") as mockMethod:
+            test_class = TestClass()
+            test_class.a_property
+            test_class.a_property
+            mockMethod.assert_called_once
